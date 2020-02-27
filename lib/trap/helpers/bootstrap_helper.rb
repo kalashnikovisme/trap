cdb03ec5c078e1a9486cf2b2e5c90833b5aa0f32
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2020-02-28 01:27:07 +0400

    Rubocop at work

--- lib/trap/helpers/bootstrap_helper.rb
+++ lib/trap/helpers/bootstrap_helper.rb
@@ -5,7 +5,7 @@ module BootstrapHelper
     path = name || path if block_given?
     options = args.extract_options!
     options[:class] = [options[:class], 'nav-link'].join(' ')
-    content_tag :li, class: "nav-item #{is_active?(path, options)}" do
+    content_tag :li, class: "nav-item #{active?(path, options)}" do
       if block_given?
         name = path
         path = options
@@ -17,43 +17,35 @@ module BootstrapHelper
   def model_menu_item(model:, route:, icon: nil, number: nil, pluralize: nil)
     title = model.is_a?(Class) ? (pluralize || model.model_name.human.pluralize(:ru)) : model
     if icon
-      menu_item route do
-        concat icon_element icon
-        concat ' '
-        concat title
-        if number && number != 0
-          concat ' '
-          concat(content_tag(:span, number.to_s, class: 'badge danger'))
-        end
-      end
+      build_menu_item_with_icon route: route, icon: icon, number: number, title: title
     else
       menu_item title, route
     end
   end
 
   def dropdown(text, icon = nil, &block)
-    content_tag :li, class: :drowdown do
-      concat(link_to('#', class: 'dropdown-toggle', data: { toggle: :dropdown }) do
-        if icon
-          concat content_tag :span, '', class: "glyphicon glyphicon-#{icon}"
-          concat ' '
-        end
-        concat text
-        unless icon
-          concat ' '
-          concat content_tag :span, '', class: :caret
-        end
-      end)
-      concat(content_tag(:ul, class: 'dropdown-menu') do
-        yield block
-      end)
+    content_tag :li, class: 'nav-item dropdown' do
+      build_a_tag_for_dropdown text: text, icon: icon, &block
     end
   end
 
+  def dropdown_item(text, link)
+    link_to(text, link, class: 'dropdown-item')
+  end
+
+  def dropdown_model_item(model:, route:, pluralize: nil)
+    title = pluralize || model.model_name.human.pluralize(:ru)
+    dropdown_item title, route
+  end
+
+  def dropdown_divider
+    content_tag(:div, class: 'dropdown-divider') do
+    end
+  end
 
   private
 
-  def is_active?(path, options = {})
+  def active?(path, options = {})
     :active if uri_state(path, options).in? %i[active chosen]
   end
 
@@ -65,16 +57,59 @@ module BootstrapHelper
                   else
                     request.path
                   end
-    if !options[:method].nil? || !options['data-method'].nil?
-      :inactive
-    elsif uri == request_uri || (options[:root] && (request_uri == '/') || (request_uri == root_url))
-      :active
-    else
-      if request_uri.start_with?(uri) && !root
-        :chosen
-      else
-        :inactive
+    active_uri_state options: options, uri: uri, request_uri: request_uri, root_url: root_url, root: root
+  end
+
+  def build_menu_item_with_icon(route:, icon:, title:, number:)
+    menu_item route do
+      concat icon_element icon
+      concat ' '
+      concat title
+      if number && number != 0
+        concat ' '
+        concat(content_tag(:span, number.to_s, class: 'badge danger'))
       end
     end
+  end
+
+  def build_a_tag_for_dropdown(text:, icon:, &block)
+    concat(link_to('#', **dropdown_link_attributes(text)) do
+      if icon
+        concat content_tag :span, '', class: "glyphicon glyphicon-#{icon}"
+        concat ' '
+      end
+      concat text
+    end)
+    concat(content_tag(:div, class: 'dropdown-menu', aria: { labelledby: text }) do
+      yield(block)
+    end)
+  end
+
+  def dropdown_link_attributes(text)
+    {
+      class: 'nav-link dropdown-toggle',
+      id: text,
+      role: :button,
+      data: { toggle: :dropdown },
+      aria: { haspopup: true, expanded: false }
+    }
+  end
+
+  def active_uri_state(options:, uri:, request_uri:, root_url:, root:)
+    if uri_state_is_inactive?(options: options)
+      :inactive
+    elsif uri_state_is_active?(request_uri: request_uri, options: options, root_url: root_url, uri: uri)
+      :active
+    else
+      request_uri.start_with?(uri) && !root ? :chosen : :inactive
+    end
+  end
+
+  def uri_state_is_inactive?(options:)
+    !options[:method].nil? || !options['data-method'].nil?
+  end
+
+  def uri_state_is_active?(request_uri:, options:, root_url:, uri:)
+    uri == request_uri || (options[:root] && (request_uri == '/') || (request_uri == root_url))
   end
 end
